ad3f510aaff00772e8010f0b6a33f1051a339439
souviitor <viitorsouza93@gmail.com>
2025-09-15 14:20:17 -0300

    inclusao de mais dois campos de calculo(das/inss), alteração de html

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def calcular_resultado(base_calculo, fixos, variavel, aliquota, reserva):
+def calcular_resultado(base_calculo, fixos, variavel, reserva, prolabore,
+                    das, inss):
     try:
         base_calculo = float(base_calculo)
         fixos = float(fixos)
         variavel = float(variavel)
-        aliquota = float(aliquota) / 100
         reserva = float(reserva) / 100
+        prolabore = float(prolabore)
+        das = float(das) / 100
+        inss = float(inss) / 100
+        # aliquota = float(aliquota) / 100
     except ValueError:
         return "Erro: Preencha todos os campos com números"
 
     custo_total = fixos + variavel
-    imposto = base_calculo * aliquota
+    das = base_calculo * das
+    inss = prolabore * inss
     valor_reserva = base_calculo * reserva
-    lucro_liquido = base_calculo - custo_total - imposto - valor_reserva
+    lucro_liquido = base_calculo - custo_total - das - inss - valor_reserva
 
     return {
         "base_calculo": base_calculo,
         "fixos": fixos,
         "variavel": variavel,
-        "imposto": imposto,
         "reserva": valor_reserva,
+        "prolabore": prolabore,
+        "das": das,
+        "inss": inss,
         "liquido": lucro_liquido
     }
 
@@ -36,11 +43,12 @@ def calcular():
         base_calculo = request.form.get("base_calculo")
         fixos = request.form.get("fixos")
         variavel = request.form.get("variavel")
-        aliquota = request.form.get("aliquota")
         reserva = request.form.get("reserva")
+        prolabore = request.form.get("prolabore")
+        das = request.form.get("das")
+        inss = request.form.get("inss")
 
-        resultado = calcular_resultado(base_calculo, fixos, variavel, aliquota,
-                                    reserva)
+        resultado = calcular_resultado(base_calculo, fixos, variavel, reserva, prolabore, das, inss)
         return render_template("resultado.html", resultado=resultado)
 
     return render_template("calcular.html")
